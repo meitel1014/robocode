51f1ff2a3fe4702322fa425209799e9257fd881c
tsuka <tsuka@LAPTOP-174AKPC0>
2017-12-06 17:49:23 +0900

    反重力移動の処理の追加・runの内容の変更

--- src/group05/G05.java
+++ src/group05/G05.java
@@ -1,6 +1,8 @@
 package group05;
 
 import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
 import robocode.HitByBulletEvent;
 import robocode.HitRobotEvent;
@@ -21,12 +23,13 @@ public class G05 extends TeamRobot{
 		setScanColor(Color.white);
 		// 各ロボットのデータリストを作成 チームメイトのデータをまず登録
 		data = new RobotDataList(getTeammates());
-
+		
 		while(true){
-			// setAhead(100);
-			// setTurnRight(90);
+			setAhead(100);
+			double direction = this.getDirection()-this.getHeadingRadians();
+			setTurnRightRadians(direction);
 			// setTurnGunRight(180);
-			// execute();
+			execute();
 		}
 	}
 
@@ -60,5 +63,31 @@ public class G05 extends TeamRobot{
 		//
 		// turnGunRight(turnGunAmt);
 		// fire(3);
+	}
+	
+	//動くべき方向をラジアンで返す
+	private double getDirection() {
+		double direction;//反重力法で導かれた移動する向き
+		double myx = this.getX();//自ロボットのｘ座標
+		double myy = this.getY();//自ロボットのｙ座標
+		double forcex,forcey;//各ロボットから受けるｘ、ｙ軸方向の力
+		double distance;//各ロボットとの距離（計算の過程で用いるだけなので値は一時的にしか保持しない）
+		double power;//各ロボットから受ける反発力（上に同じ）
+		Point2D.Double posi;//各ロボットの座標を保存（上に同じ）
+		
+		ArrayList<RobotData> list = data.getEnemies();
+		
+		forcex = 0;
+		forcey = 0;
+		
+		for(RobotData info : list) {
+			posi = info.getPosition();
+			distance = Math.sqrt( Math.pow((myx - posi.getX()),2) + Math.pow((myy + posi.getY()),2));
+			power = info.getDefendPoint() / distance;
+			forcex += power * ((myx - posi.getX())/distance);
+			forcey += power * ((myy - posi.getY())/distance);
+		}
+		direction = Math.acos(forcex/Math.sqrt(forcex*forcex+forcey*forcey));
+		return direction;
 	}
 }
