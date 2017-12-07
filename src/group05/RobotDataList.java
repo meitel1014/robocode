b5209e72d1f24d51cf83aaeb790fc3915e32f1d3
ukyoriki <ukyoriki@DESKTOP-ED98APB>
2017-12-08 00:57:02 +0900

    防御ポイント

--- src/group05/RobotDataList.java
+++ src/group05/RobotDataList.java
@@ -2,6 +2,9 @@ package group05;
 
 import java.util.ArrayList;
 
+import robocode.HitByBulletEvent;
+import robocode.ScannedRobotEvent;
+
 /**
  * 各ロボットに対する{@link RobotData}を格納するクラス．
  * @author MEITEL
@@ -52,4 +55,36 @@ public class RobotDataList{
 		}
 		return ret;
 	}
+
+	//敵の弾に当たった時に防御ポイントを1上げる
+	public void onHitByBullet(HitByBulletEvent e){
+		RobotData robo = new RobotData(e.getName(),true);//とりあえずtrue代入したが、良いのか？
+		if(robo.isTeammate() == false)
+			robo.addDefendpoint(1);//攻撃をしてきた相手の防御ポイントを1上げる
+	}
+
+	//スキャンしたときにこちらを向いているときだけ防御ポイントを1上げる
+	public void onScannedRobot(ScannedRobotEvent e) {
+		RobotData robo = new RobotData(e.getName(),true);
+		if(robo.isTeammate() == false) {
+			int count=0;//2回目以降のスキャンでもカウントすることが無いようにしたいが、毎回定義してそう、、、
+			if(e.getBearing() >160 || e.getBearing() <-160 ) {
+				if(count == 0) {
+					robo.addDefendpoint(1);
+					count = 1;
+				}
+				else {
+					if(count == 1) {
+						robo.subDefendpoint(1);
+						count = 0;
+					}
+				}
+			}
+		}
+	}
+
+
 }
+
+
+
