3f13fd722824348f5446dda01952c1224d01f58a
tsuka <tsuka@LAPTOP-174AKPC0>
2017-12-09 12:44:51 +0900

    反重力修正

--- src/group05/G05.java
+++ src/group05/G05.java
@@ -9,23 +9,22 @@ import robocode.HitRobotEvent;
 import robocode.ScannedRobotEvent;
 import robocode.TeamRobot;
 
-public class G05 extends TeamRobot {
+public class G05 extends TeamRobot{
 	int dist = 50; // あたったときに逃げる距離
 	boolean movingForward; //
 	RobotDataList data;
-	int wallpoint = 2; //壁の重力
+	int wallpoint = 10; // 壁の重力
 
-	public void run() {
+	public void run(){
 		data = new RobotDataList(getTeammates());
-		setBodyColor(Color.black);
+		setBodyColor(Color.pink);
 		setGunColor(Color.blue);
 		setRadarColor(Color.yellow);
 		setBulletColor(Color.red);
 		setScanColor(Color.white);
 		// 各ロボットのデータリストを作成 チームメイトのデータをまず登録
 
-
-		while (true) {
+		while(true){
 			setAhead(100);
 			double direction = this.getDirection() - this.getHeadingRadians();
 			setTurnRightRadians(direction);
@@ -34,42 +33,36 @@ public class G05 extends TeamRobot {
 		}
 	}
 
-	//スキャンしたときにこちらを向いているときだけ防御ポイントを1上げる
-	public void onScannedRobot(ScannedRobotEvent e) {
+	// スキャンしたときにこちらを向いているときだけ防御ポイントを1上げる
+	public void onScannedRobot(ScannedRobotEvent e){
 		RobotData robo = data.get(e.getName());
-		if (robo.isTeammate() == false) {
-			int count = 0;//2回目以降のスキャンでもカウントすることが無いようにしたいが、毎回定義されてそう
-			if (e.getBearing() > 160 || e.getBearing() < -160) {
+
+		if(robo.isTeammate() == false){
+			if(e.getBearing() > 160 || e.getBearing() < -160){
 				robo.setDirectionDefendpoint(1);
 				robo.setDirectionAttackPoint(2);
-				count = 1;
-			} else {
-				if (count == 1) {
-					robo.setDirectionAttackPoint(1);
-					robo.setDirectionDefendpoint(0);
-					count = 0;
-				}
+			}else{
+				robo.setDirectionAttackPoint(1);
+				robo.setDirectionDefendpoint(0);
 			}
-			if(e.getDistance() <= 300) {
+			if(e.getDistance() <= 300){
 				robo.setDistanceAttackPoint(3);
-			}
-			else if(e.getDistance() > 300 && e.getDistance() <= 600) {
+			}else if(e.getDistance() > 300 && e.getDistance() <= 600){
 				robo.setDistanceAttackPoint(2);
-			}
-			else {
+			}else{
 				robo.setDistanceAttackPoint(1);
 			}
 		}
 	}
 
-	//敵の弾に当たった時に防御ポイントを1上げる
-	public void onHitByBullet(HitByBulletEvent e) {
+	// 敵の弾に当たった時に防御ポイントを1上げる
+	public void onHitByBullet(HitByBulletEvent e){
 		RobotData robo = data.get(e.getName());
-		if (robo.isTeammate() == false)
-			robo.addBulletDefendpoint(1);//攻撃をしてきた相手の防御ポイントを1上げる
+		if(robo.isTeammate() == false)
+			robo.addBulletDefendpoint(1);// 攻撃をしてきた相手の防御ポイントを1上げる
 	}
 
-	public void onHitRobot(HitRobotEvent e) {
+	public void onHitRobot(HitRobotEvent e){
 		// double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() +
 		// getHeading() - getGunHeading());
 		//
@@ -77,58 +70,61 @@ public class G05 extends TeamRobot {
 		// fire(3);
 	}
 
-	//動くべき方向をラジアンで返す
-	private double getDirection() {
-		double direction;//反重力法で導かれた移動する向き
-		double myx = this.getX();//自ロボットのｘ座標
-		double myy = this.getY();//自ロボットのｙ座標
-		double forcex, forcey;//各ロボットから受けるｘ、ｙ軸方向の力
-		double distance;//各ロボットとの距離（計算の過程で用いるだけなので値は一時的にしか保持しない）
-		double power;//各ロボットから受ける反発力（上に同じ）
-		Point2D.Double posi;//各ロボットの座標を保存（上に同じ）
+	// 動くべき方向をラジアンで返す
+	private double getDirection(){
+		double direction;// 反重力法で導かれた移動する向き
+		double myx = this.getX();// 自ロボットのｘ座標
+		double myy = this.getY();// 自ロボットのｙ座標
+		double forcex, forcey;// 各ロボットから受けるｘ、ｙ軸方向の力
+		double distance;// 各ロボットとの距離（計算の過程で用いるだけなので値は一時的にしか保持しない）
+		double power;// 各ロボットから受ける反発力（上に同じ）
+		Point2D.Double posi;// 各ロボットの座標を保存（上に同じ）
 
-		ArrayList<RobotData> list = data.getAll();
+		ArrayList<RobotData> list = data.getEnemies();
 
 		forcex = 0;
 		forcey = 0;
 
-		
 		/*
 		 * 敵ロボットとの反重力
 		 */
-		for(RobotData info : list) {
+		for(RobotData info: list){
 			posi = info.getPosition();
-			distance = Math.sqrt(Math.pow((myx - posi.getX()), 2) + Math.pow((myy + posi.getY()), 2));
+			distance = Math.sqrt(Math.pow((myx - posi.getX()), 2) + Math.pow((myy - posi.getY()), 2));
 			power = info.getDefendPoint() / distance;
 			forcex += power * ((myx - posi.getX()) / distance);
 			forcey += power * ((myy - posi.getY()) / distance);
 		}
-		
+
 		/*
 		 * 壁との反重力
 		 */
 		forcex += wallpoint / myx;
-		forcex += wallpoint / (1000 - myx);
+		forcex += wallpoint / (myx - 1000);
 		forcey += wallpoint / myy;
-		forcey += wallpoint / (800 - myy);
-		
+		forcey += wallpoint / (myy - 800);
+
 		/*
 		 * ベクトルから方向への変換
 		 */
-		if((forcex != 0) || (forcey != 0)) {
-			direction = Math.acos(forcex/Math.sqrt(forcex*forcex+forcey*forcey));
-		}else {
+		if((forcex != 0) || (forcey != 0)){
+			if(forcey>=0) {
+				direction = Math.acos(forcex / Math.sqrt(forcex * forcex + forcey * forcey));
+			}else {
+				direction = -Math.acos(forcex / Math.sqrt(forcex * forcex + forcey * forcey));
+			}
+		}else{
 			direction = 0;
 		}
 		direction = changeDistanceForRobo(direction);
 		return direction;
 	}
-	
+
 	/*
 	 * 数学角度からrobocodeの角度への変換
 	 */
-	private double changeDistanceForRobo(double radian) {
-		double direction = -(radian-(Math.PI/2));
+	private double changeDistanceForRobo(double radian){
+		double direction = -(radian - (Math.PI / 2));
 		return direction;
 	}
 }
